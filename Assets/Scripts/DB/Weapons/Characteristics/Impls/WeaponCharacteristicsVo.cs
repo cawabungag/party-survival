0cfdf466510b374c789ffa01f3fac23cd405e50e
rafik <Bloodhand18@gmail.com>
2020-10-12 16:32:17 +0300

    Добавил мета данные характеристик оружия и префабов

---
+++ Assets/Scripts/DB/Weapons/Characteristics/Impls/WeaponCharacteristicsVo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class WeaponCharacteristicsVo : MonoBehaviour
+{
+   private string _weaponId;
+   private int _damage;                     //Урон
+   private int _firingFrequency;            //Частота стрельбы
+   private int _shells;                     //Патроны в обойме
+   
+   public string WeaponId => _weaponId;
+   public int Damage => _damage;
+   public int FiringFrequency => _firingFrequency;
+   public int Shells => _shells;
+}
