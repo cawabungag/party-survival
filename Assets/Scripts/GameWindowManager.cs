823fdbaff54fca7fc397ac8a1c1204073f8f103a
Ilya <38839317+cawabungag@users.noreply.github.com>
2020-10-16 14:06:30 +0300

    Update GameWindowManager.cs

--- Assets/Scripts/GameWindowManager.cs
+++ Assets/Scripts/GameWindowManager.cs
@@ -5,7 +5,7 @@ using UI.Game.Windows;
 using UnityEngine;
 using Zenject;
 
-public abstract class GameWindowManager : IInitializable
+public class GameWindowManager : IInitializable
 {
 	private readonly SignalBus _signalBus;
 	private readonly GameContext m_gameContext;
