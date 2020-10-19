c52487929e16ebb60ec237bbda3d6c24d07eaa75
rafik <Bloodhand18@gmail.com>
2020-10-19 17:09:51 +0300

    Added instantiate enemy strategy

---
+++ Assets/Scripts/Services/Unit/Impls/Strategies/AInstantiateEnemyStrategy.cs
@@ -0,0 +1,39 @@
+﻿using DB.Units.EnemyUnit.Prefabs;
+using DB.Units.Prefabs;
+using Ecs.View;
+using Ecs.View.Impls.Game;
+using UnityEngine;
+using Zenject;
+
+namespace Services.Unit.Impls.Strategies
+{
+    public abstract class AInstantiateEnemyStrategy : IInstantiateUnitStrategy
+    {
+        private readonly DiContainer _diContainer;
+        private readonly IEnemyPrefabDatabase _enemyDatabase;
+
+        protected AInstantiateEnemyStrategy(
+
+            IEnemyPrefabDatabase enemyDatabase
+        )
+        {
+            _enemyDatabase = enemyDatabase;
+        }
+
+        public abstract bool CanInstantiate(GameEntity entity);
+
+        public virtual ILinkable Create(GameEntity entity)
+        {
+            var enemyType = entity.ecsGameObjectType.Value;
+            var enemy = _enemyDatabase.GetZombiePrefab(enemyType);
+            var unit = _enemyDatabase.GetZombiePrefab(enemyType);
+            var view = _diContainer.InstantiatePrefabForComponent<GameObjectView>(unit.Prefab, entity.ecsGamePosition.value,
+
+            Quaternion.identity, null);
+			
+            return view;
+        }
+    }
+
+   
+}
---
+++ Assets/Scripts/Services/Unit/Impls/Strategies/InstantiateEnemyStrategy.cs
@@ -0,0 +1,47 @@
+﻿using DB.Units;
+using DB.Units.Characteristics;
+using DB.Units.Characteristics.Impls;
+using DB.Units.EnemyUnit.Characteristics;
+using DB.Units.EnemyUnit.Characteristics.Impls;
+using DB.Units.EnemyUnit.Prefabs;
+using DB.Units.Prefabs;
+using Ecs.View;
+using Services.Unit.Impls.Strategies;
+using Zenject;
+
+
+public class InstantiateEnemyStrategy : AInstantiateEnemyStrategy 
+{
+        private readonly IEnemyCharacteristicsDatabase _enemyCharacteristicDatabase;
+        private readonly IEnemyPrefabDatabase _enemyPrefabDatabase;
+
+
+        public InstantiateEnemyStrategy(
+            
+            IEnemyCharacteristicsDatabase enemyCharacteristicDatabase,
+            IEnemyPrefabDatabase enemyPrefabDatabase
+        ) : base(enemyPrefabDatabase)
+        {
+            _enemyCharacteristicDatabase = enemyCharacteristicDatabase;
+        }
+
+        public override bool CanInstantiate(GameEntity entity) => entity.isEcsGameFlagsUnit;
+
+        public override ILinkable Create(GameEntity entity)
+        {
+            var linkable = base.Create(entity);
+            var unitType = entity.ecsGameObjectType.Value;
+            var setting = _enemyCharacteristicDatabase.GetCharacteristics(unitType);
+            InitializeNonSerializableParameters(entity, setting);
+            return linkable;
+        }
+
+        private void InitializeNonSerializableParameters(GameEntity entity, EnemyCharacteristicVo setting)
+        {
+            entity.AddEcsGameUnitsMaxHealth(setting.Health);
+            entity.AddEcsGameUnitsHealth(setting.Health);
+            entity.AddEcsGameUnitsSpeed(setting.Speed);
+            entity.AddEcsGameUnitsRangeView(setting.RangeView);
+        }
+    
+}
