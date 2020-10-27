e9d4e5a8c26035d883dd88cc6842d046f207bae9
rafik <Bloodhand18@gmail.com>
2020-10-27 18:04:51 +0300

    MovementDatabase

---
+++ Assets/Scripts/DB/Units/MovementType/MovementSpeedType/IMovementSpeedType.cs
@@ -0,0 +1,9 @@
+﻿using DB.Units.MovementType.MovementSpeedType.Impls;
+
+namespace DB.Units.MovementType.MovementSpeedType
+{
+    public interface IMovementSpeedType
+    {
+        MovementSpeedTypeVo GetMovementSpeed(EMovementType moveSpeed);
+    }
+}
---
+++ Assets/Scripts/DB/Units/MovementType/MovementSpeedType/Impls/MovementSpeedTypeDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace DB.Units.MovementType.MovementSpeedType.Impls
+{
+    [CreateAssetMenu(menuName = "Installers/MovementSpeedDatabase", fileName = "UMovementSpeedDatabase")]
+    public class MovementSpeedTypeDatabase : ScriptableObject, IMovementSpeedType
+    {
+        [SerializeField] private MovementSpeedTypeVo[] _movementSpeedTypeVos;
+        
+        public MovementSpeedTypeVo GetMovementSpeed(EMovementType moveSpeed)
+        {
+            for (int i = 0; i < _movementSpeedTypeVos.Length; i++)
+            {
+                MovementSpeedTypeVo movementSpeedTypeVo = _movementSpeedTypeVos[i];
+                if (movementSpeedTypeVo.SpeedType == moveSpeed)
+                {
+                    return movementSpeedTypeVo;
+                }
+            }
+
+            throw new ArgumentException(
+                $"[{nameof(MovementSpeedTypeDatabase)}] No MovementSpeedTypeVo for SpeedType:{moveSpeed}");
+        }
+        
+    }
+}
